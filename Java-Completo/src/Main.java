99fdd5124ed48deec7bf1c43a5d29d2835792e29
Adiuleson Arlen <adiulesonarlen.ads@gmail.com>
2025-03-07 22:06:29 -0300

    Condicionais em Java

---
+++ Java-Completo/src/Main.java
@@ -0,0 +1,18 @@
+public class Main {
+    public static void main(String[] args) {
+        byte b = 100;
+        short s = 10000;
+        int i = 100000;
+        long l = 100000L;
+        float f = 10.5f;
+        char c = 'A';
+        boolean bool = true;
+        String str = "Adam";
+
+        if(b < 99) {
+            System.out.println("Verdadeiro!");
+        } else {
+            System.out.println("Falso!");
+        }
+    }
+}
